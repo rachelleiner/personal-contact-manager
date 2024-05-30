9a622a37cbe53abc1bbeb942d7b9072f196cabd5
Lily Yarbrough <lyrenee02@gmail.com>
2024-05-30 14:53:09 -0400

    updated

---
+++ Add.php
@@ -0,0 +1,55 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$Email = $inData["Email"];
+	$Phone = $inData["Phone"];
+	$ID = $inData["ID"];
+
+	$conn = new mysqli("localhost", "Access-20", "WeLoveCOP4331-20", "contactmanager"); 
+
+	if($FirstName == "")
+	{
+		returnWithError("Please provide a name");
+	}
+
+	else if($Phone == "")
+	{
+		returnWithError("Please provide a phone number");
+	}
+
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (FirstName,LastName,Email,Phone,ID) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("ssssi", $FirstName, $LastName,$Email,$Phone,$ID);
+		
+		$stmt->execute();
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
