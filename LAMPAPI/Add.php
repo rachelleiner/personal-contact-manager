a2f71c80fcb588a4a3411e6009eac46ce2d302a2
mxKaZVN <67602990+mxKaZVN@users.noreply.github.com>
2024-05-30 16:15:43 -0400

    Add.php changes from office hours

--- LAMPAPI/Add.php
+++ LAMPAPI/Add.php
@@ -1,28 +1,13 @@
 <?php
 	$inData = getRequestInfo();
 	
-	$FirstName = $inData["FirstName"];
-	$LastName = $inData["LastName"];
-	$Email = $inData["Email"];
-	$Phone = $inData["Phone"];
-	$ID = $inData["ID"];
-
-	$conn = new mysqli("contactmanager.xyz", "Access-20", "WeLoveCOP4331-20", "contactmanager"); 
-
-	if($FirstName == "" && $LastName == "")
-	{
-		returnWithError("Please provide a name");
-	}
-
-	else if($Email == "")
-	{
-		returnWithError("Please provide an email");
-	}
-
-	else if($Phone == "")
-	{
-		returnWithError("Please provide a phone number");
-	}
+	$FirstName = $inData["A_FirstName"];
+	$LastName = $inData["A_LastName"];
+  	$PhoneNumber = $inData["A_PhoneNumber"];
+  	$Email = $inData["A_Email"];
+  	$ID = $inData["ID"];
+ 
+	$conn = new mysqli("localhost", "Access-20", "WeLoveCOP4331-20", "contactmanager"); 
 
 	if ($conn->connect_error) 
 	{
@@ -31,8 +16,9 @@
 
 	else
 	{
+		
 		$stmt = $conn->prepare("INSERT into Contacts (FirstName,LastName,Email,Phone,ID) VALUES(?,?,?,?,?)");
-		$stmt->bind_param("ssssi", $FirstName, $LastName,$Email,$Phone,$ID);
+		$stmt->bind_param("ssssi",$FirstName,$LastName,$Email,$PhoneNumber,$ID);
 		$stmt->execute();
 
 
