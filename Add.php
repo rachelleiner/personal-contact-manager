a379210d5bde602dac7c1c628220d2a45285a42c
Emily Gensch <em845191@ucf.edu>
2024-05-30 22:14:29 -0400

    Update Add.php
    Add.php is now functional

--- Add.php
+++ Add.php
@@ -1,36 +1,28 @@
-<?php
-	$inData = getRequestInfo();
-	
-	$A_FirstName = $inData["A_FirstName"];
-	$A_LastName = $inData["A_LastName"];
-  	$A_PhoneNumber = $inData["A_PhoneNumber"];
-  	$A_Email = $inData["Email"];
-  	$ID = $inData["ID"];
- 
+<?php	
+  
+	$inData = getRequestInfo();   
+	$FirstName = $inData["AFirstName"]; 
+	$LastName = $inData["ALastName"];
+	$PhoneNumber = $inData["APhone"];
+	$Email = $inData["AEmail"];
+	$ID = $inData["userId"];
+   
 	$conn = new mysqli("localhost", "Access-20", "WeLoveCOP4331-20", "contactmanager"); 
+ 
 
 	if ($conn->connect_error) 
 	{
-		returnWithError( $conn->connect_error );
+		returnWithError( $conn->connect_error );  
 	} 
-
 	else
-	{
-		if($A_FirstName == "")
-		{
-			returnWithError("Please provide a name");
-		}
-
-		else if($A_Phone == "")
-		{
-			returnWithError("Please provide a phone number");
-		}
-
-		$stmt = $conn->prepare("INSERT into Contacts (FirstName,LastName,Email,Phone,ID) VALUES(?,?,?,?,?)");
-		$stmt->bind_param("ssssi",$A_FirstName,$A_LastName,$A_Email,$A_Phone,$ID);
+	{	
+		$stmt = $conn->prepare("INSERT into Contacts (FirstName,LastName,Email,Phone,UserID) VALUES(?,?,?,?,?)");
+		
+    $stmt->bind_param("ssssi",$FirstName,$LastName,$Email,$PhoneNumber,$ID);
+   
 		$stmt->execute();
 
-
+    
 		$stmt->close();
 		$conn->close();
 	}
@@ -54,7 +46,7 @@
 
 	function returnWithInfo( $FirstName, $LastName, $ID )
 	{
-		$retValue = '{"ID":' . $ID . ',"FirstName":"' . $FirstName . '","LastName":"' . $LastName . '","error":""}';
+		$retValue = '{"UserID":' . $ID . ',"FirstName":"' . $FirstName . '","LastName":"' . $LastName . '","error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
 	
